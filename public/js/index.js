6e98f0ecb6d776eac96b4480d1c353b157ed9e93
Bladen Ward <Bladenward1213@gmail.com>
2024-05-09 21:00:57 -0500

    basic sort viz

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,48 @@
 import { shuffle } from './utils/shuffle.js';
 
-const n = 100;
-const array = []
+const n = 50; // Number of bars
+let barArray = []
 
-for (let i = 0; i < n; i++) {
-    array.push(i + 1);
-}
-shuffle(array);
+init();
 
-for(let i = 0; i < array.length; i++) {
-    const bar = document.createElement('div');
-    bar.style.height = array[i] + "%";
-    bar.style.width = "10px";
-    bar.style.backgroundColor = "black";
-    bars.appendChild(bar);
+function init() {
+    barArray = [];
+    for (let i = 0; i < n; i++) barArray.push(i + 1); 
+    shuffle(barArray);
+    displayBars(barArray)
 }
+
+function play() {
+    bubbleSort(barArray);
+    displayBars(barArray);
+}
+
+function bubbleSort(array) {
+    do {
+        var swapped = false;
+        for(let i = 1; i < array.length; i++) {
+            if(array[i] < array[i - 1]) {
+                swapped = true;
+                [array[i - 1], array[i]] = [array[i], array[i - 1]];
+            }
+        }
+    } while (swapped)
+}
+
+function displayBars(array) {
+    bars.innerHTML = "";
+    for(let i = 0; i < array.length; i++) {
+        const bar = document.createElement('div');
+        bar.style.height = array[i] * 2 + "%";
+        bar.classList.add('bar');
+        bars.appendChild(bar);
+    }
+}
+
+document.getElementById('generateBars').addEventListener('click', () => {
+    init();
+})
+
+document.getElementById('sort').addEventListener('click', () => {
+    play();
+})
